0791966925d6dc472d69b85f5529d9091165279d
kinkinxii <kinkinxii@gmail.com>
2025-04-12 17:56:09 +0900

    add

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,9 @@
 import { createOpenAI } from '@ai-sdk/openai';
 import { streamText } from 'ai';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
@@ -7,6 +11,31 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+  const secret_name = "openai-api-key";
+
+  const client = new SecretsManagerClient({
+    region: "ap-northeast-1", // Change to your region
+  });
+
+  let response;
+
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = response.SecretString;
+
+  const parsedSecret = secret ? JSON.parse(secret) : {};
+  const OPENAI_API_KEY = parsedSecret[secret_name] ?? '';
+  
   const { messages } = await req.json();
 
   const openai = createOpenAI({
